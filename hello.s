aae3da1a9fc3c8a3ae43cd7929c680e5deb8cab6
christiantomato <christianntamayo@gmail.com>
2025-09-04 12:04:32 -0400

    hello world in assembly

---
+++ hello.s
@@ -0,0 +1,39 @@
+/*
+Hello World Program for arm64 assembly on macOS
+ */
+
+
+
+//specify the starting function (main)
+.global _main
+
+//the entry point for the program
+_main: 
+    b _print
+    b _exit
+
+//function for writing to terminal
+_print:
+    //system call number for write is 4 (3 arguments: writing to where, address of message, length of message)
+    mov x16, #4
+    //1 means standard output to the terminal
+    mov x0, #1
+    //address of string
+    adr x1, message
+    //length of string
+    mov x2, #12
+    //invoke kernel
+    svc 0
+
+//function for exiting the program with exit code 0
+_exit:
+    //move system call number for exit into x8 (syscall number is 1)
+    mov x16, #1
+    //x0 register takes argument for exit number (0 for success)
+    mov x0, #0
+    //invoke kernel
+    svc 0
+
+//store the hello world message in ram (12 characters)
+message: 
+    .ascii "hello world\n"
